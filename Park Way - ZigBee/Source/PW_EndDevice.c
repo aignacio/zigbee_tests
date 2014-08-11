31283c42ca41793d5b70629cc07e4930486e513c
Ânderson Alienware <anderson.igns@gmail.com>
2014-08-11 10:24:55 -0300

    Last version with the ultrasound constant adjusted

---
+++ Park Way - ZigBee/Source/PW_EndDevice.c
@@ -0,0 +1,568 @@
+/*****************************************************************************
+  Project: Park Way (ZigBee)
+  End Device Firmware
+
+  Ver.: 1.0
+  Date: 31/07/14
+  Developer:  Ânderson Ignácio da Silva
+  Obs.:
+    -LCD_SUPPORTED=DEBUG or LCD_SUPPORTED must be disable in the CC2530 China 
+    modules to work
+    
+    -BEFORE PROGRAMMING is important to ERASE the FLASH memory with the FLASH
+    programmer because the NV_RESTORE option is enabled  to restore the  End 
+    Devices after the coordinator power down
+
+    -When the board has PA_LNA RANGE EXTENDER, it must be necessary to cut off
+    the 'x' from the defines of #define xHAL_PA_LNA of the hal_board_cfg.h
+
+  Pre-processor directives:
+    NWK_AUTO_POLL
+    ZTOOL_P1
+    MT_TASK
+    MT_SYS_FUNC
+    MT_ZDO_FUNC
+    xLCD_SUPPORTED=DEBUG
+    xPOWER_SAVING
+
+*****************************************************************************/
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "OSAL.h"
+#include "AF.h"
+#include "ZDApp.h"
+#include "ZDObject.h"
+#include "ZDProfile.h"
+#include "ParkWay.h"
+#include "DebugTrace.h"
+#include "OnBoard.h"
+/* HAL */
+#include "hal_lcd.h"
+#include "hal_led.h"
+#include "hal_key.h"
+#include "hal_uart.h"
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * CONSTANTS
+ */
+#define   DEVICE_PW_1                  '0'     //(first char)Tag of the device 
+#define   DEVICE_PW_2                  '1'       //(second char)Tag of the device
+#define   ParkWay_SEND_MSG_TIMEOUT     800     //Time to send data to the coordinator(poll ultrasound-ms)
+#define   TIME_TO_CONNECT_COORD       1000     //Time to connect (auto bind) the device to the coord(ms).
+#define   PIN_TRIGGER                 0x02     //Ultrasonic pin trigger - P0.1
+#define   PIN_ECHO                    0x04     //Ultrasonic pin trigger - P0.2
+#define   DISTANCE_CONSTANT          40000     //Distance measured by the ultrasonic
+#define   PIN_GREEN_LED               0x08     //P0.3
+#define   PIN_RED_LED                 0x10     //P0.4
+#define   TIME_ULTRA                   200     //Poll time to read ultrasonic
+#define   DEBUG_ULTRA                          //Used to send ultrasound read to the AP
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+// This list should be filled with Application specific Cluster IDs.
+const cId_t ParkWay_ClusterList[ParkWay_MAX_CLUSTERS] =
+{
+  ParkWay_CLUSTERID
+};
+
+const SimpleDescriptionFormat_t ParkWay_SimpleDesc =
+{
+  ParkWay_ENDPOINT,              //  int Endpoint;
+  ParkWay_PROFID,                //  uint16 AppProfId[2];
+  ParkWay_DEVICEID,              //  uint16 AppDeviceId[2];
+  ParkWay_DEVICE_VERSION,        //  int   AppDevVer:4;
+  ParkWay_FLAGS,                 //  int   AppFlags:4;
+  ParkWay_MAX_CLUSTERS,          //  byte  AppNumInClusters;
+  (cId_t *)ParkWay_ClusterList,  //  byte *pAppInClusterList;
+  ParkWay_MAX_CLUSTERS,          //  byte  AppNumInClusters;
+  (cId_t *)ParkWay_ClusterList   //  byte *pAppInClusterList;
+};
+
+uint8 vaga='0';
+uint8 fifo_ultra[12]={"000000000000"};
+uint8 index_flag=0;
+char dist[5];
+uint8 flag_send=0;
+
+// This is the Endpoint/Interface description.  It is defined here, but
+// filled-in in ParkWay_Init().  Another way to go would be to fill
+// in the structure here and make it a "const" (in code space).  The
+// way it's defined in this sample app it is define in RAM.
+endPointDesc_t ParkWay_epDesc;
+
+/*********************************************************************
+ * EXTERNAL VARIABLES
+ */
+
+/*********************************************************************
+ * EXTERNAL FUNCTIONS
+ */
+
+/*********************************************************************
+ * LOCAL VARIABLES
+ */
+byte ParkWay_TaskID;   // Task ID for internal task/event processing
+                          // This variable will be received when
+                          // ParkWay_Init() is called.
+devStates_t ParkWay_NwkState;
+
+
+byte ParkWay_TransID;  // This is the unique message ID (counter)
+
+afAddrType_t ParkWay_DstAddr;
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+void SendUART(uint8 *data,uint8 len);
+void uart_init(void);
+static void ParkWay_ProcessZDOMsgs( zdoIncomingMsg_t *inMsg );
+static void ParkWay_MessageMSGCB( afIncomingMSGPacket_t *pckt );
+static void ParkWay_SendTheMessage( void );
+void read_ultrasonic(void);
+void control_fifo(uint16 ultra);
+/*********************************************************************
+ * NETWORK LAYER CALLBACKS
+ */
+
+/*********************************************************************
+ * PUBLIC FUNCTIONS
+ */
+
+//Initialization function for the Park Way task. This is called during initialization and should contain any application specific initialization (ie. hardware initialization/setup, table initialization, power up notificaiton ... )
+void ParkWay_Init( uint8 task_id )
+{
+  ParkWay_TaskID = task_id;
+  ParkWay_NwkState = DEV_INIT;
+  ParkWay_TransID = 0;
+
+  // Device hardware initialization can be added here or in main() (Zmain.c).
+  // If the hardware is application specific - add it here.
+  // If the hardware is other parts of the device add it in main().
+
+  ParkWay_DstAddr.addrMode = (afAddrMode_t)AddrNotPresent;
+  ParkWay_DstAddr.endPoint = 0;
+  ParkWay_DstAddr.addr.shortAddr = 0;
+
+  // Fill out the endpoint description.
+  ParkWay_epDesc.endPoint = ParkWay_ENDPOINT;
+  ParkWay_epDesc.task_id = &ParkWay_TaskID;
+  ParkWay_epDesc.simpleDesc
+            = (SimpleDescriptionFormat_t *)&ParkWay_SimpleDesc;
+  ParkWay_epDesc.latencyReq = noLatencyReqs;
+
+  // Register the endpoint description with the AF
+  afRegister( &ParkWay_epDesc );
+
+  // Register for all key events - This app will handle all key events
+  RegisterForKeys( ParkWay_TaskID );
+
+  // Update the display
+#if defined ( LCD_SUPPORTED )
+  HalLcdWriteString( "ParkWay-Peer", HAL_LCD_LINE_1 );
+#endif
+
+  ZDO_RegisterForZDOMsg( ParkWay_TaskID, End_Device_Bind_rsp );
+  ZDO_RegisterForZDOMsg( ParkWay_TaskID, Match_Desc_rsp );
+
+  osal_start_timerEx( ParkWay_TaskID,
+                      POLLING_OF_DEV_EVT,
+                      TIME_TO_CONNECT_COORD);
+}
+
+//Main function to proccess the timers and events 
+uint16 ParkWay_ProcessEvent( uint8 task_id, uint16 events )
+{
+  afIncomingMSGPacket_t *MSGpkt;
+  //afDataConfirm_t *afDataConfirm;
+  zAddrType_t dstAddr;
+
+  // Data Confirmation message fields
+  //byte sentEP;
+  //ZStatus_t sentStatus;
+  //byte sentTransID;       // This should match the value sent
+  (void)task_id;  // Intentionally unreferenced parameter
+
+  if ( events & SYS_EVENT_MSG )
+  {
+    MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( ParkWay_TaskID );
+    while ( MSGpkt )
+    {
+      switch ( MSGpkt->hdr.event )
+      {
+        case ZDO_CB_MSG: //Event trigger by ZigBee Device Object like connect, bind...etc
+          ParkWay_ProcessZDOMsgs( (zdoIncomingMsg_t *)MSGpkt );
+          break;
+
+        case AF_DATA_CONFIRM_CMD: //Event trigger by a confirmation of sent message
+          // This message is received as a confirmation of a data packet sent.
+          // The status is of ZStatus_t type [defined in ZComDef.h]
+          // The message fields are defined in AF.h
+          // afDataConfirm = (afDataConfirm_t *)MSGpkt;
+          // sentEP = afDataConfirm->endpoint;
+          // sentStatus = afDataConfirm->hdr.status;
+          // sentTransID = afDataConfirm->transID;
+          // (void)sentEP;
+          // (void)sentTransID;
+
+          // // Action taken when confirmation is received.
+          // if ( sentStatus != ZSuccess )
+          // {
+          //   // The data wasn't delivered -- Do something
+          // }
+          break;
+
+        case AF_INCOMING_MSG_CMD: //Event trigger by a receive message
+          ParkWay_MessageMSGCB( MSGpkt );
+          break;
+
+        case ZDO_STATE_CHANGE: //Event trigger by a connection to the coordinator or Router
+          ParkWay_NwkState = (devStates_t)(MSGpkt->hdr.status);
+          if ( (ParkWay_NwkState == DEV_ZB_COORD)
+              || (ParkWay_NwkState == DEV_ROUTER)
+              || (ParkWay_NwkState == DEV_END_DEVICE) )
+          {
+            // Start sending the status message in a regular interval.
+            osal_start_timerEx( ParkWay_TaskID,
+                                ParkWay_SEND_MSG_EVT,
+                                ParkWay_SEND_MSG_TIMEOUT );
+            // Start reading ultrasonic in a regular interval.
+            osal_start_timerEx( ParkWay_TaskID,
+                                POLLING_ULTRASONIC,
+                                TIME_ULTRA);
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      // Release the memory
+      osal_msg_deallocate( (uint8 *)MSGpkt );
+
+      // Next
+      MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( ParkWay_TaskID );
+    }
+
+    // return unprocessed events
+    return (events ^ SYS_EVENT_MSG);
+  }
+
+  // Send to the coordinator the status of ultrasound
+  if ( events & ParkWay_SEND_MSG_EVT )
+  {
+    // Send "the" message
+    ParkWay_SendTheMessage();
+    
+    // Setup to send message again
+    osal_start_timerEx( ParkWay_TaskID,
+                        ParkWay_SEND_MSG_EVT,
+                        ParkWay_SEND_MSG_TIMEOUT );
+
+    // return unprocessed events
+    return (events ^ ParkWay_SEND_MSG_EVT);
+  }
+  
+  if(events & POLLING_ULTRASONIC)
+  {
+    osal_start_timerEx( ParkWay_TaskID,
+                        POLLING_ULTRASONIC,
+                        TIME_ULTRA);
+    read_ultrasonic();
+  }
+  
+  if(events & POLLING_OF_DEV_EVT) //This force the auto bind to the coordinator or Router
+  {
+    //HalLedSet ( HAL_LED_1, HAL_LED_MODE_ON);
+    // Initiate a Match Description Request (Service Discovery)
+    dstAddr.addrMode = AddrBroadcast;
+    dstAddr.addr.shortAddr = NWK_BROADCAST_SHORTADDR;
+    ZDP_MatchDescReq( &dstAddr, NWK_BROADCAST_SHORTADDR,
+                      ParkWay_PROFID,
+                      ParkWay_MAX_CLUSTERS, (cId_t *)ParkWay_ClusterList,
+                      ParkWay_MAX_CLUSTERS, (cId_t *)ParkWay_ClusterList,
+                      FALSE );
+    osal_start_timerEx( ParkWay_TaskID,
+                        POLLING_OF_DEV_EVT,
+                        TIME_TO_CONNECT_COORD);
+  } 
+
+
+  HalLedSet( HAL_LED_ALL, HAL_LED_MODE_OFF ); //Shutdown all LEDS to reduce consumption of power
+  
+  // Discard unknown events
+  return 0;
+}
+
+/*********************************************************************
+ * Event Generation Functions
+ */
+
+/*********************************************************************
+ */
+//This function process the ZDO events to bind
+static void ParkWay_ProcessZDOMsgs( zdoIncomingMsg_t *inMsg )
+{
+  switch ( inMsg->clusterID )
+  {
+    case End_Device_Bind_rsp:
+      if ( ZDO_ParseBindRsp( inMsg ) == ZSuccess )
+      {
+        // Light LED
+        //HalLedSet ( HAL_LED_2, HAL_LED_MODE_FLASH );
+      }
+      break;
+
+    case Match_Desc_rsp:
+      {
+        ZDO_ActiveEndpointRsp_t *pRsp = ZDO_ParseEPListRsp( inMsg );
+        if ( pRsp )
+        {
+          if ( pRsp->status == ZSuccess && pRsp->cnt )
+          {
+            ParkWay_DstAddr.addrMode = (afAddrMode_t)Addr16Bit;
+            ParkWay_DstAddr.addr.shortAddr = pRsp->nwkAddr;
+            // Take the first endpoint, Can be changed to search through endpoints
+            ParkWay_DstAddr.endPoint = pRsp->epList[0];
+
+            // Light LED
+            //HalLedSet( HAL_LED_2, HAL_LED_MODE_ON );
+
+            //This stop the peer from send request do the coordinator
+            //HalLedSet ( HAL_LED_1, HAL_LED_MODE_OFF);
+            osal_stop_timerEx( ParkWay_TaskID,POLLING_OF_DEV_EVT);
+          }
+          osal_mem_free( pRsp );
+        }
+      }
+      break;
+  }
+}
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+//This function process the frames received by the application framework (source routers or the coordinator)
+
+static void ParkWay_MessageMSGCB( afIncomingMSGPacket_t *pkt_1 )
+{
+  /************************************************* This was used to send some specific data do the last device connected to the COORDINATOR (HDA)**************************************/
+  // uint8* pkt;
+  // pkt=(pkt_1->cmd.Data);
+  
+  // uint8 pin_ultra=4;
+  
+  // P0SEL &= (~BV(2));
+  // P0DIR |= (BV(2));
+    
+  // if(*pkt==48||*(pkt+1)==48||*(pkt+2)==48)
+  // {
+  //  P0&=~pin_ultra;
+  // }
+  // else
+  // {
+  //   P0|=pin_ultra;
+  // }
+  /************************************************* This was used to send some specific data do the last device connected to the COORDINATOR (HDA)**************************************/
+}
+
+//This function send the status of own park to the coordinator
+static void ParkWay_SendTheMessage( void )
+{
+  #ifdef DEBUG_ULTRA
+    uint8 pData[11];
+    pData[0] = 'E';
+    pData[1] = 'D';
+    pData[2] = DEVICE_PW_1;
+    pData[3] = DEVICE_PW_2;
+    pData[4] = '-';
+    pData[5] = vaga;
+    pData[6] = dist[0];
+    pData[7] = dist[1];
+    pData[8] = dist[2];
+    pData[9] = dist[3];
+    pData[10] = dist[4];
+  #endif
+    // uint8 pData[6];
+    // pData[0] = 'E';
+    // pData[1] = 'D';
+    // pData[2] = DEVICE_PW_1;
+    // pData[3] = DEVICE_PW_2;
+    // pData[4] = '-';
+    // pData[5] = vaga;
+
+ 
+  //Send data to the coordinator ADDRESS(0000)
+  if ( AF_DataRequest( &ParkWay_DstAddr, &ParkWay_epDesc,
+                       ParkWay_CLUSTERID,
+                       (byte)osal_strlen( pData ) + 1,
+                       (byte *)&pData,
+                       &ParkWay_TransID,
+                       AF_DISCV_ROUTE, AF_DEFAULT_RADIUS ) == afStatus_SUCCESS )
+  {
+    // Successfully requested to be sent.
+  }
+  else
+  {
+    osal_stop_timerEx( ParkWay_TaskID,
+                        ParkWay_SEND_MSG_EVT);
+    osal_start_timerEx( ParkWay_TaskID,
+                        POLLING_OF_DEV_EVT,
+                        200);
+  }
+}
+
+
+static void UART_CallBack(uint8 port, uint8 event)
+{
+  (void)port;
+  uint8 cmdbuff[1];
+  if (event & (HAL_UART_RX_FULL | HAL_UART_RX_ABOUT_FULL | HAL_UART_RX_TIMEOUT))
+  {
+    HalUARTRead(port, cmdbuff, 1);
+    SendUART(cmdbuff,1);
+  }
+}
+
+
+//This function initializes the UART
+void uart_init()
+{
+  halUARTCfg_t uartConfig;
+  uartConfig.configured = TRUE; // 2x30 don't care - see uart driver.
+  uartConfig.baudRate = HAL_UART_BR_9600;
+  uartConfig.flowControl = TRUE;
+  uartConfig.flowControlThreshold = 48; // 2x30 don't care - see uart driver.
+  uartConfig.rx.maxBufSize = 128; // 2x30 don't care - see uart driver.
+  uartConfig.tx.maxBufSize = 128; // 2x30 don't care - see uart driver.
+  uartConfig.idleTimeout = 6; // 2x30 don't care - see uart driver.
+  uartConfig.intEnable = TRUE; // 2x30 don't care - see uart driver.
+  uartConfig.callBackFunc = UART_CallBack;
+  HalUARTOpen (HAL_UART_PORT_0, &uartConfig);
+}
+
+void control_fifo(uint16 ultra)
+{
+
+  uint8 i,empty=0,full=0;
+  
+  if(ultra>DISTANCE_CONSTANT)
+  {
+    fifo_ultra[index_flag]='0';
+  }
+  else
+  {
+    fifo_ultra[index_flag]='1';
+  }
+
+  if(index_flag==11)
+  {
+    for(i=0;i<12;i++)
+    {
+      if(fifo_ultra[i]=='1')  full++;
+      else if (fifo_ultra[i]=='0')  empty++;
+    }
+  }
+
+  if(empty!=full)
+  { 
+    if(empty>full)  vaga='0';
+    else  vaga='1';
+  }
+  
+  if(vaga=='1')
+  {
+    P0 &= ~PIN_RED_LED; 
+    Onboard_wait(1); 
+    P0 |= PIN_GREEN_LED;
+  }
+  else
+  {
+    P0 &= ~PIN_GREEN_LED; 
+    Onboard_wait(1); 
+    P0 |= PIN_RED_LED;
+  }
+
+  if(index_flag>=12) 
+    index_flag = 0;
+  else
+    index_flag++;
+
+}
+
+void read_ultrasonic()
+{
+    uint16 i,timeout_1=0,timeout_2=0;
+
+    P0SEL &= ~(PIN_GREEN_LED+PIN_RED_LED);
+    P0DIR |= PIN_GREEN_LED+PIN_RED_LED;
+
+
+    //Configure trigger
+    P0SEL &= ~PIN_TRIGGER;   //P0_1 GPIO
+    P0DIR |= PIN_TRIGGER;    //P0_1 output
+    //Onboard_wait(10);
+    P0SEL &= ~PIN_ECHO;   //P0_2 GPIO
+    P0DIR &= ~PIN_ECHO;   //P0_2 input
+    Onboard_wait(1);
+
+    if(!(P0&PIN_ECHO))
+    {
+      P0 &= ~PIN_TRIGGER;       //------> 0
+      Onboard_wait(6);
+      P0 |= PIN_TRIGGER;        //------> 1
+      Onboard_wait(6);      //------> 6us
+      P0 &= ~PIN_TRIGGER;       //------> 0 
+      Onboard_wait(6); 
+      //&&(timeout_1<40000)
+      while(!(P0&PIN_ECHO)&&(timeout_2<100))
+      {
+        if(timeout_1<65000)
+        { 
+          timeout_1++;
+        }
+        else
+        {
+          timeout_2++;
+          timeout_1 = 0;
+        }
+      } 
+      
+      while(P0&PIN_ECHO)
+      {
+        if(i<65530) 
+          i++; 
+        else 
+          break;
+      }
+          
+      control_fifo(i);
+
+      #ifdef DEBUG_ULTRA
+        dist[0] = (i/10000) + 0x30;
+        dist[1] = (i%10000)/1000 + 0x30;
+        dist[2] = (((i%10000)%1000)/100) + 0x30;
+        dist[3] = ((((i%10000)%1000)%100)/10) +0x30; 
+        dist[4] = ((((i%10000)%1000)%100)%10) + 0x30;
+      #endif
+    }
+}
+
+
+void SendUART(uint8 *data,uint8 len)
+{
+  HalUARTWrite(HAL_UART_PORT_0, data, len);
+}
+
+
+/*********************************************************************/
